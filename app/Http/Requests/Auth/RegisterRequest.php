294282397619d58fec9b978f38eec02f34d5925d
Gift <giftgabanakgosi@gmail.com>
2025-02-02 17:46:15 +0200

    added authentication cycle logic to the controllers

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, array<int, mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'userType' => ['required', 'string', 'in:client,provider'],
+            'phone' => ['required', 'string', 'max:20'],
+        ];
+    }
+}
